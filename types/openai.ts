49222f41cfb7c0afbf743862a81a4f5fb2b735ba
Le Wang <lewang.dev@gmail.com>
2023-08-02 16:49:46 +0800

    add more model for openai

--- types/openai.ts
+++ types/openai.ts
@@ -9,8 +9,14 @@ export interface OpenAIModel {
 
 export enum OpenAIModelID {
   GPT_3_5 = 'gpt-3.5-turbo',
+  GPT_3_5_0613 = 'gpt-3.5-turbo-0613',
+  GPT_3_5_0301 = 'gpt-3.5-turbo-0301',
+  GPT_3_5_16K = 'gpt-3.5-turbo-16k',
+  GPT_3_5_16K_0613 = 'gpt-3.5-turbo-16k-0613',
   GPT_3_5_AZ = 'gpt-35-turbo',
   GPT_4 = 'gpt-4',
+  GPT_4_0613 = 'gpt-4-0613',
+  GPT_4_0314 = 'gpt-4-0314',
   GPT_4_32K = 'gpt-4-32k',
 }
 
@@ -24,6 +30,30 @@ export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
     maxLength: 12000,
     tokenLimit: 4000,
   },
+  [OpenAIModelID.GPT_3_5_0613]: {
+    id: OpenAIModelID.GPT_3_5_0613,
+    name: 'GPT-3.5-0613',
+    maxLength: 12000,
+    tokenLimit: 4000,
+  },
+  [OpenAIModelID.GPT_3_5_0301]: {
+    id: OpenAIModelID.GPT_3_5_0301,
+    name: 'GPT-3.5-0301',
+    maxLength: 12000,
+    tokenLimit: 4000,
+  },
+  [OpenAIModelID.GPT_3_5_16K]: {
+    id: OpenAIModelID.GPT_3_5_16K,
+    name: 'GPT-3.5-16k',
+    maxLength: 48000,
+    tokenLimit: 16000,
+  },
+  [OpenAIModelID.GPT_3_5_16K_0613]: {
+    id: OpenAIModelID.GPT_3_5_16K_0613,
+    name: 'GPT-3.5-16k-0613',
+    maxLength: 48000,
+    tokenLimit: 16000,
+  },
   [OpenAIModelID.GPT_3_5_AZ]: {
     id: OpenAIModelID.GPT_3_5_AZ,
     name: 'GPT-3.5',
@@ -33,6 +63,18 @@ export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
   [OpenAIModelID.GPT_4]: {
     id: OpenAIModelID.GPT_4,
     name: 'GPT-4',
+    maxLength: 24000,
+    tokenLimit: 8000,
+  },
+  [OpenAIModelID.GPT_4_0613]: {
+    id: OpenAIModelID.GPT_4_0613,
+    name: 'GPT-4-0613',
+    maxLength: 24000,
+    tokenLimit: 8000,
+  },
+  [OpenAIModelID.GPT_4_0314]: {
+    id: OpenAIModelID.GPT_4_0314,
+    name: 'GPT-4-0314',
     maxLength: 24000,
     tokenLimit: 8000,
   },
